8419c8a6e16b2873df881058a9dda24b52835137
James Dibnah <jamesdibnah@gmail.com>
2018-07-01 23:17:04 +0100

    Adding levels

--- Assets/Scripts/Environment/SceneChanger.cs
+++ Assets/Scripts/Environment/SceneChanger.cs
@@ -4,11 +4,9 @@ using UnityEngine.SceneManagement;
 namespace Environment {
 	public class SceneChanger : MonoBehaviour {
 		public int nextLevel;
-		[SerializeField] private AudioSource m_portalSound;
-
+	
 		public void NextLevel() {
-			SceneManager.LoadSceneAsync(nextLevel);
-			m_portalSound.Play();
+			SceneManager.LoadSceneAsync(nextLevel);		
 		}
 
 		public void ResetLevel() {
---
+++ Assets/Scripts/Environment/ShowMouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Penut");
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+    
+	}
+}
